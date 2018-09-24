a9eb1657c5da62474be5de6b0f74f1b52ded8e5b
Austin Osborne <austin.osborne@capitalone.com>
2018-09-24 15:43:40 -0500

    Added initial pass for creating tables in Postgres

--- Phase 2/team132_p2_schema.sql
+++ Phase 2/team132_p2_schema.sql
@@ -1,45 +1,63 @@
+-- Our database will be using the Postgres engine.
+
 -- Tables
-CREATE TABLE User (
-  email VARCHAR(255) NOT NULL,
+DROP TABLE IF EXISTS "User";
+CREATE TABLE "User" (
+  email VARCHAR(255) PRIMARY KEY NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  PIN VARCHAR(4) NOT NULL,
-  PRIMARY KEY (email)
+  PIN VARCHAR(4) NOT NULL
 );
 
-CREATE TABLE Corkboard (
-
-);
-
-CREATE TABLE Pushpin (
-  id INT(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS "Pushpin";
+CREATE TABLE "Pushpin" (
+  id SERIAL PRIMARY KEY NOT NULL,
   description TEXT,
   image_link TEXT NOT NULL,
   time_added TIMESTAMP NOT NULL
-  PRIMARY KEY (id)
 );
 
-CREATE TABLE Comment (
-  id INT(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS "Corkboard";
+CREATE TABLE "Corkboard" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS "Comment";
+CREATE TABLE "Comment" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  pushpin_id INTEGER,
   text TEXT NOT NULL,
-  time_added TIMESTAMP NOT NULL,
-  PRIMARY KEY (id)
+  time_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Tag (
-
+DROP TABLE IF EXISTS "Tag";
+CREATE TABLE "Tag" (
+  text TEXT NOT NULL,
+  pushpin_id INTEGER NOT NULL,
+  PRIMARY KEY(text, pushpin_id)
 );
 
-CREATE TABLE Like (
-
+DROP TABLE IF EXISTS "Like";
+CREATE TABLE "Like" (
+  user_email VARCHAR(255) NOT NULL,
+  pushpin_id INTEGER NOT NULL,
+  PRIMARY KEY(user_email, pushpin_id)
 );
 
-CREATE TABLE Follow (
-
+DROP TABLE IF EXISTS "Follow";
+CREATE TABLE "Follow" (
+  user_email VARCHAR(255) NOT NULL,
+  followed_user_email varchar(255) NOT NULL,
+  PRIMARY KEY(user_email, followed_user_email)
 );
 
-CREATE TABLE Watch (
-
+DROP TABLE IF EXISTS "Watch";
+CREATE TABLE "Watch" (
+  user_email VARCHAR(255) NOT NULL,
+  corkboard_id INTEGER NOT NULL,
+  PRIMARY KEY(user_email, corkboard_id)
 );
 
 -- Constraints
