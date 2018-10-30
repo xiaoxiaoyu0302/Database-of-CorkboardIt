57ac11e9711e7c2529b1a4620970d7fe8a87c96d
Austin Osborne <ffosb22@Austins-MacBook-Pro.local>
2018-10-30 16:27:17 -0500

    Added template for recently updated corkboards

--- Phase 3/Corkboard/src/routes.py
+++ Phase 3/Corkboard/src/routes.py
@@ -1,18 +1,18 @@
-from flask import render_template, flash, redirect, url_for, g
+from flask import render_template, flash, redirect, url_for, session, request
 from src import app
 from src.forms import LoginForm
 from app import get_db
+from psycopg2.extras import RealDictCursor
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    title = 'Title test'
-    posts = [
-        { 'author': 'Austin', 'body': 'Body test' },
-        { 'author': 'Jon', 'body': 'Another post' }
-    ]
-    return render_template('index.html', title=title, posts=posts)
+    db = get_db()
+    cursor = db.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(open('src/sql/recent_updates.sql').read())
+    updates = cursor.fetchall()
+    return render_template('index.html', updates=updates, corkboards=[])
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -20,12 +20,21 @@ def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         db = get_db()
-        cursor = db.cursor()
+        cursor = db.cursor(cursor_factory=RealDictCursor)
         cursor.execute(open('src/sql/login.sql', 'r').read().format(email=login_form.email.data,
                                                                     pin=login_form.pin.data))
-        if len(cursor.fetchone()) == 0:
+        user = cursor.fetchone()
+        if user is None:
             flash("Unable to login. Try again.")
         else:
-            flash("Login successful.")
-        return redirect(url_for('index'))
+            session.logged_in_user = user
+            return redirect(url_for('index'))
     return render_template('login.html', form=login_form)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in_user', None)
+    login_form = LoginForm
+    return render_template('login.html', form=login_form)
+
