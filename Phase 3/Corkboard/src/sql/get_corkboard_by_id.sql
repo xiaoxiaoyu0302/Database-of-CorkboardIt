d3d9e2943260e28da431b706dcf25d619614dfcd
Jon Moen <jmoen6@gatech.edu>
2018-11-07 07:59:17 -0800

    Fleshing out corkboard page. Fixing css for visilibility radio button.

--- Phase 3/Corkboard/src/sql/get_corkboard_by_id.sql
+++ Phase 3/Corkboard/src/sql/get_corkboard_by_id.sql
@@ -1,6 +1,32 @@
 SELECT
-    *
+	"Corkboard".title,
+	CONCAT( "User".first_name, ' ' , "User".last_name) AS owner_name,
+	(
+		SELECT
+			"Pushpin".time_added
+		FROM
+			"Pushpin"
+		WHERE
+			"Pushpin".corkboard_id = %s
+		GROUP BY
+			"Pushpin".time_added
+		ORDER BY
+			"Pushpin".time_added DESC
+		LIMIT 1
+	) AS last_update,
+	(
+		SELECT
+			COUNT (*)
+		FROM
+			"Watched"
+		WHERE
+			"Watched".corkboard_id = %s
+	) AS watcher_count
 FROM
-    "Corkboard"
+	"Corkboard"
+INNER JOIN
+	"User"
+ON
+	"User".email = "Corkboard".user_email
 WHERE
     "Corkboard".id = %s;
